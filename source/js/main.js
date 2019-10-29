e940dc3b2ae60597de140be6a0ebfbd30ad18ca7
Anna Kavarnukaeva <a.kavarnukaeva@gmail.com>
2019-10-29 14:16:38 +0300

    Добавляет разметку и стили для десктопной и планшетной версии блока Услуги

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,20 @@
-// 'use strict';
+'use strict';
+
+(function () {
+
+  var toggles = document.querySelectorAll('.accordeon__button');
+
+  [].forEach.call(toggles, function (toggle) {
+    toggle.addEventListener('click', function () {
+      var item = toggle.closest('.accordeon__item');
+
+      if (item.classList.contains('accordeon__item--opened')) {
+        item.classList.remove('accordeon__item--opened');
+      } else {
+        item.classList.add('accordeon__item--opened');
+      }
+    });
+  });
 // var pageHeader = document.querySelector('.page-header');
 // var headerToggle = document.querySelector('.page-header__toggle');
 //
@@ -13,3 +29,5 @@
 //     pageHeader.classList.remove('page-header--opened');
 //   }
 // });
+
+})();
