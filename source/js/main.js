00bb765791b8bc4b3ea15ea4d378a0be3e77fcba
Anna Kavarnukaeva <a.kavarnukaeva@gmail.com>
2019-11-11 15:13:40 +0300

    Удаляет закомментированный код

--- source/js/main.js
+++ source/js/main.js
@@ -112,21 +112,4 @@
   };
 
   IMask(element, maskOptions);
-
-
-// var pageHeader = document.querySelector('.page-header');
-// var headerToggle = document.querySelector('.page-header__toggle');
-//
-// pageHeader.classList.remove('page-header--nojs');
-//
-// headerToggle.addEventListener('click', function () {
-//   if (pageHeader.classList.contains('page-header--closed')) {
-//     pageHeader.classList.remove('page-header--closed');
-//     pageHeader.classList.add('page-header--opened');
-//   } else {
-//     pageHeader.classList.add('page-header--closed');
-//     pageHeader.classList.remove('page-header--opened');
-//   }
-// });
-
 })();
